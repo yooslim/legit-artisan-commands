af32a4632a69f76e45f80fe7a6f0f85c88f7c7d5
YOo Slim <slimani.youcef.09@gmail.com>
2022-09-15 00:48:53 +0100

    Code review
    Fixed code indentation

--- src/Models/ConsoleToken.php
+++ src/Models/ConsoleToken.php
@@ -26,10 +26,10 @@ class ConsoleToken extends Model
      */
     protected $hidden = [
         'value',
-		'expires_at',
+	'expires_at',
     ];
-
-	/**
+	
+    /**
      * The attributes that should be cast to native types.
      *
      * @var array
@@ -45,36 +45,39 @@ class ConsoleToken extends Model
     protected static function boot()
     {
         parent::boot();
+
         static::creating(function (ConsoleToken $model) {
             $model->value = Str::random(config('legit-artisan-commands.token.size', 8));
             $model->expires_at = now()->addSeconds(config('legit-artisan-commands.token.lifetime', 60));
         });
     }
 
-	/**
-	 * Create relationship with users table
-	 */
-	public function user(): BelongsTo
+    /**
+     * Create relationship with users table
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
     {
         return $this->belongsTo(config('legit-artisan-commands.relationships.user'), 'user_id');
     }
 
-	/**
+    /**
      * Define setter and getter for value.
      *
      * @return Attribute
      */
     public function value(): Attribute
-	{
+    {
         return Attribute::make(
             get: fn (string $value) => $value,
-			set: function (string $value) {
-				if (empty($value)) {
-					throw new InvalidArgumentException();
-				}
+            set: function (string $value) {
+            	if (empty($value)) {
+            		throw new InvalidArgumentException();
+            	}
 
-				return $value;
-			},
+            	return $value;
+            },
         );
     }
 }
