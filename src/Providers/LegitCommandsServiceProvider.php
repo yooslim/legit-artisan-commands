9a760cb3cd5e8383f61d0f838ee027c50b5df07d
YOo Slim <slimani.youcef.09@gmail.com>
2022-09-15 00:52:21 +0100

    Code review
    Fixed code indentation

--- src/Providers/LegitCommandsServiceProvider.php
+++ src/Providers/LegitCommandsServiceProvider.php
@@ -8,42 +8,42 @@ use YOoSlim\LegitArtisanCommands\Utils\LegitArtisanCommandHandler;
 
 class LegitCommandsServiceProvider extends ServiceProvider
 {
-	/**
-	 * Register any package services.
-	 */
-	public function register()
-	{
-		/* */
-	}
+    /**
+     * Register any package services.
+     */
+    public function register()
+    {
+        /* */
+    }
 
-	/**
-	 * Bootstrap any package services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
         // Publish config file
         $this->publishes([
             __DIR__.'/../../config/legit-artisan-commands.php' => config_path('legit-artisan-commands.php'),
         ]);
 
-		// Load migrations files on app boot
-		$this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+        // Load migrations files on app boot
+        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
 
         // Load translations files on app boot
         $this->loadTranslationsFrom(__DIR__.'/../../lang', 'legit-artisan-commands');
 
-		// Register facades
-		$this->app->bind('legitArtisanCommand', function() {
-			return new LegitArtisanCommandHandler();
-		});
+        // Register facades
+        $this->app->bind('legitArtisanCommand', function() {
+            return new LegitArtisanCommandHandler();
+        });
 
         // Register commands
         if ($this->app->runningInConsole()) {
-			$this->commands([
-				ArtisanAuthenticationCommand::class,
-			]);
-		}
-	}
+            $this->commands([
+                ArtisanAuthenticationCommand::class,
+            ]);
+        }
+    }
 }
