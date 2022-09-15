61210e54fcfa26dea450a7f58817878a25d9562c
YOo Slim <slimani.youcef.09@gmail.com>
2022-09-15 01:06:35 +0100

    Code review / convert tab to spaces

--- src/Utils/Traits/LegitArtisanCommandSignature.php
+++ src/Utils/Traits/LegitArtisanCommandSignature.php
@@ -4,15 +4,15 @@ namespace YOoSlim\LegitArtisanCommands\Utils\Traits;
 
 trait LegitArtisanCommandSignature
 {
-	/**
+    /**
      * Create a new command instance.
      *
      * @return void
      */
     public function __construct()
     {
-		$this->signature .= '{--token= : Token for authentication }';
-		
+        $this->signature .= '{--token= : Token for authentication }';
+        
         parent::__construct();
     }
 }
